438dd172878ac1025ca9d8710ca0d5298dcef360
John Patrick Ryan D. Mandal <JohnPatrickRyan.Mandal@cebupacificair.com>
2025-03-03 17:19:24 +0800

    wip: sidebar profile

--- src/app/components/shared/Sidebar/index.tsx
+++ src/app/components/shared/Sidebar/index.tsx
@@ -1,8 +1,17 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import {
+  Calendar,
+  ChevronUp,
+  Home,
+  Inbox,
+  Search,
+  Settings,
+  User2,
+} from "lucide-react";
 
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarHeader,
@@ -10,6 +19,15 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { auth } from "@/lib/auth";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { getFirstLetter } from "@/lib/helper";
 
 // Menu items.
 const items = [
@@ -41,6 +59,7 @@ const items = [
 ];
 
 export async function AppSidebar() {
+  const session = await auth();
   return (
     <Sidebar>
       <SidebarHeader className='px-4 py-4'>
@@ -64,6 +83,49 @@ export async function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <div className='flex items-center space-x-2'>
+                    <div>
+                      <Avatar className='hover:cursor-pointer rounded-full'>
+                        <AvatarImage src={session?.user?.image ?? " "} />
+                        <AvatarFallback>
+                          {session?.user?.name &&
+                            getFirstLetter(session.user.name)}
+                        </AvatarFallback>
+                      </Avatar>
+                    </div>
+
+                    <div className='text-sm font-semibold'>
+                      {session?.user?.name}
+                    </div>
+                  </div>
+
+                  <ChevronUp className='ml-auto' />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side='right'
+                className='w-[--radix-popper-anchor-width]'
+              >
+                <DropdownMenuItem>
+                  <span>Account</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Billing</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Sign out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 }
